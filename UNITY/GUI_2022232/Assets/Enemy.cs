29d2b89e2b7b8e3ae7c02a8a873e38e1fe43c2a7
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-03-11 15:44:54 +0100

    Turret aims and shoots
    Using particle system

---
+++ UNITY/GUI_2022232/Assets/Enemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("I got shot");
+    }
+}
---
+++ UNITY/GUI_2022232/Assets/Tower/TurretTargeting.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTargeting : MonoBehaviour
+{
+    [SerializeField] private Transform head;
+    [SerializeField] private ParticleSystem particleSys;
+    [SerializeField] private float range = 30f;
+
+    private Transform target = null;
+
+    private void Awake()
+    {
+       //particleSys.Play();
+    }
+
+    private void Update()
+    {
+        GetTarget();
+        TargetEnemy();
+    }
+
+    private void GetTarget()
+    {
+        //min value search
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        target = null;
+        float maxDistance = Mathf.Infinity;
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float distance = Vector3.Distance(transform.position, enemies[0].transform.position);
+            if (distance < range && distance < maxDistance)
+            {
+                target = enemies[0].transform;
+            }
+        }
+
+    }
+    private void TargetEnemy()
+    {
+        if (target == null)
+        {
+            Shoot(false);
+        }
+        else
+        {
+            head.LookAt(target);
+            Shoot(true);
+        }
+
+        
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = particleSys.emission;
+        emissionModule.enabled = isActive;
+    }
+}
