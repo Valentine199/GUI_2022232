16e8b38dec38a54a8362c82065b598765bf995ac
infopek <10saero01@gmail.com>
2023-03-13 13:19:03 +0100

    Setup basic timed spawning for enemies (demo)

--- UNITY/GUI_2022232/Assets/Scripts/WaveManager.cs
+++ UNITY/GUI_2022232/Assets/Scripts/WaveManager.cs
@@ -8,21 +8,27 @@ public class WaveManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Instantiate(_enemies[0]);
         Instantiate(_spline);
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
+        InvokeRepeating(SPAWN_METHOD, _spawnTime, _spawnDelay);
     }
 
     private void SpawnEnemy()
     {
-
+        Instantiate(_enemies[0], transform.position, transform.rotation);
+        if (_stopSpawning)
+            CancelInvoke(SPAWN_METHOD);
     }
+
+    private const string SPAWN_METHOD = "SpawnEnemy";
+
+    // Enemy spawn pattern, a wave is always the same
+    private string _pattern;
 
     [SerializeField] private List<Enemy> _enemies;
     [SerializeField] private SplineContainer _spline;
+
+
+    [SerializeField] private bool _stopSpawning;
+    [SerializeField] private float _spawnTime;
+    [SerializeField] private float _spawnDelay;
 }
