03ce063e83266774d165e3140928f69588c621cd
Karesz <karcsibalazs@gmail.com>
2023-03-18 22:30:13 +0100

    Add jump functionality

--- UNITY/GUI_2022232/Assets/Scripts/PlayerController.cs
+++ UNITY/GUI_2022232/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityT.Manager;
 
 namespace UnityT.PlayerControl
@@ -14,6 +16,10 @@ namespace UnityT.PlayerControl
         [SerializeField] private float UpperLimit = -40f;
         [SerializeField] private float BottomLimit = 70f;
         [SerializeField] private float MouseSensitivity = 21.9f;
+        [SerializeField] private float JumpFactor = 260f;
+        [SerializeField] private float DistanceToGround = 0.85f;
+        [SerializeField] private float AirResistance = 0.8f;
+        [SerializeField] private LayerMask GroundCheck;        
 
         private Rigidbody _playerRigidbody;
 
@@ -27,10 +33,17 @@ namespace UnityT.PlayerControl
 
         private int _yVelHash;
 
+        private int _zVelHash;
+
+        private int _jumpHash;
+
+        private bool _grounded = false;
+
+        private int _groundHash;
+
+        private int _fallingHash;
+
         private float _xRotation;
-        private float _yRotation;
-
-
 
 
         private const float _walkSpeed = 2f;
@@ -47,11 +60,17 @@ namespace UnityT.PlayerControl
 
             _xVelHash = Animator.StringToHash("X_Velocity");
             _yVelHash = Animator.StringToHash("Y_Velocity");
+            _jumpHash = Animator.StringToHash("Jump");
+            _groundHash = Animator.StringToHash("Grounded");
+            _fallingHash = Animator.StringToHash("Falling");
+            _zVelHash = Animator.StringToHash("Z_Velocity");
         }
 
         private void FixedUpdate()
         {
+            SampleGround();
             Move();
+            HandleJump();
         }
 
         private void LateUpdate()
@@ -66,14 +85,22 @@ namespace UnityT.PlayerControl
             float tartgetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
             if (_inputManager.Move == Vector2.zero) tartgetSpeed = 0;
 
-            _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * tartgetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
-            _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * tartgetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
+            if (_grounded)
+            {
+                _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * tartgetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
+                _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * tartgetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
 
-            var xVelDifference = _currentVelocity.x - _playerRigidbody.velocity.x;
-            var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.z;
+                var xVelDifference = _currentVelocity.x - _playerRigidbody.velocity.x;
+                var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.z;
 
-            _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0, zVelDifference)), ForceMode.VelocityChange);
+                _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0, zVelDifference)), ForceMode.VelocityChange);
 
+            }
+            else
+            {
+                _playerRigidbody.AddForce(transform.TransformVector(new Vector3(_currentVelocity.x* AirResistance,0,_currentVelocity.y * AirResistance)),ForceMode.VelocityChange);
+            }
+            
             _animator.SetFloat(_xVelHash, _currentVelocity.x);
             _animator.SetFloat(_yVelHash, _currentVelocity.y);
 
@@ -92,6 +119,46 @@ namespace UnityT.PlayerControl
 
             Camera.localRotation = Quaternion.Euler(_xRotation,0,0);
             _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * MouseSensitivity * Time.smoothDeltaTime, 0));
+        }
+
+        private void HandleJump()
+        {
+            if (!_hasAnimator) return;
+            if (!_inputManager.Jump) return;
+            if (!_grounded) return;            
+            _animator.SetTrigger(_jumpHash);       
+        }
+            
+        public void JumpAddForce()
+        {
+            _playerRigidbody.AddForce(-_playerRigidbody.velocity.y*Vector3.up,ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            _animator.ResetTrigger(_jumpHash);
+        }
+
+        private void SampleGround()
+        {
+            if (!_hasAnimator) return;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(_playerRigidbody.worldCenterOfMass, Vector3.down, out hitInfo, DistanceToGround + 0.1f, GroundCheck))
+            {
+                //Grounded
+                _grounded = true;
+                SetAnimationGrounding();
+                return;
+            }
+            //Falling
+            _grounded = false;
+            _animator.SetFloat(_zVelHash, _playerRigidbody.velocity.y);
+            SetAnimationGrounding();
+            return;
+        }
+
+        private void SetAnimationGrounding()
+        {
+            _animator.SetBool(_fallingHash, !_grounded);
+            _animator.SetBool(_groundHash, _grounded);
         }
     }
 }
