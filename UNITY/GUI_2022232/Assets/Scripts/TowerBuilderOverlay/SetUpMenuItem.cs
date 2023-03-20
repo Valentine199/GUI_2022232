8b2320e96b03209cd5875ae41fdf04539cba6362
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-03-20 08:09:37 +0100

    The menuItem can name itself

---
+++ UNITY/GUI_2022232/Assets/Scripts/TowerBuilderOverlay/SetUpMenuItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetUpMenuItem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _ItemName;
+    //Something, something picture change
+    private void Start()
+    {
+        HandleOnClick script =  gameObject.GetComponent<HandleOnClick>();
+
+        _ItemName.text = script.MyTower.name;
+    }
+}
