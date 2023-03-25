5126f63bb6d8c145d7240cf7070cfe7ab61e6b4f
infopek <10saero01@gmail.com>
2023-03-25 16:41:04 +0100

    Create EnemyType enum

---
+++ UNITY/GUI_2022232/Assets/Scripts/Data/Enemies/EnemyType.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense.Data.Enemies
+{
+    public enum EnemyType
+    {
+        rockhopper,
+        chinstrap,
+        adelie,
+        macaroni,
+        magellanic,
+        gentoo,
+        king,
+        emperor
+    }
+}
