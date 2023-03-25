b0303c7f979fd11ae91a5cf616944cd34b3553ba
infopek <10saero01@gmail.com>
2023-03-25 17:00:59 +0100

    Create PathController script that handles the waypoints on the path

---
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Path/PathController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense.Gameplay.Path
+{
+    public class PathController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            
+        }     
+
+        public Vector3 this[int i] => _waypoints[i].transform.position;
+
+        public int WaypointCount => _waypoints?.Count ?? 0;
+
+        public float WaypointSize => _waypointSize;
+
+        [SerializeField] private List<GameObject> _waypoints;
+        [SerializeField] private float _waypointSize;
+    }
+}
