a890e4f273daafadf27a14d009780bb15e66988d
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-03-30 11:55:28 +0200

    GetEnemyPosition now returns Transforms

--- UNITY/GUI_2022232/Assets/Scripts/Gameplay/Helpers/GetEnemyPosition.cs
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Helpers/GetEnemyPosition.cs
@@ -8,43 +8,43 @@ namespace TowerDefense.Gameplay.Helpers
 {
     public static class GetEnemyPosition
     {
-        public static Vector3 First(List<EnemyController> enemies)
+        public static Transform First(List<EnemyController> enemies)
         {
             var farthest = enemies[0];
             for (int i = 1; i < enemies.Count; i++)
                 farthest = EnemyController.CompareFirst(farthest, enemies[i]);
 
-            return farthest.transform.position;
+            return farthest.transform;
         }
 
-        public static Vector3 Last(List<EnemyController> enemies)
+        public static Transform Last(List<EnemyController> enemies)
         {
             var last = enemies[0];
             for (int i = 1; i < enemies.Count; i++)
                 last = EnemyController.CompareLast(last, enemies[i]);
 
-            return last.transform.position;
+            return last.transform;
         }
 
-        public static Vector3 Strongest(List<EnemyController> enemies)
+        public static Transform Strongest(List<EnemyController> enemies)
         {
             var strongest = enemies[0];
             for (int i = 1; i < enemies.Count; i++)
                 strongest = EnemyController.CompareStrongest(strongest, enemies[i]);
 
-            return strongest.transform.position;
+            return strongest.transform;
         }
 
-        public static Vector3 Weakest(List<EnemyController> enemies)
+        public static Transform Weakest(List<EnemyController> enemies)
         {
             var weakest = enemies[0];
             for (int i = 1; i < enemies.Count; i++)
                 weakest = EnemyController.CompareWeakest(weakest, enemies[i]);
 
-            return weakest.transform.position;
+            return weakest.transform;
         }
 
-        public static Vector3 Closest(List<EnemyController> enemies, Vector3 towerPosition)
+        public static Transform Closest(List<EnemyController> enemies, Vector3 towerPosition)
         {
             var closest = enemies[0];
             var closestDist = Vector3.Distance(towerPosition, closest.transform.position);
@@ -58,7 +58,7 @@ namespace TowerDefense.Gameplay.Helpers
                 }
             }
 
-            return closest.transform.position;
+            return closest.transform;
         }
     }
 }
