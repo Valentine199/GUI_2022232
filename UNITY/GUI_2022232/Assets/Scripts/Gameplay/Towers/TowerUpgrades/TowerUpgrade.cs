89e0b62d21c19d1abbd5ddf845420794eae40f4a
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-03-30 11:56:51 +0200

    Tower upgrade system added

---
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Towers/TowerUpgrades/TowerUpgrade.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Data.Towers;
+using TowerDefense.Gameplay.Core;
+using TowerDefense.Towers.TowerAttackControllers;
+using TowerDefense.Towers.TowerEnums;
+using TowerDefense.Towers.TowerUpgrades.Interfaces;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace TowerDefense.Towers.TowerUpgrades
+{
+    public class TowerUpgrade : MonoBehaviour
+    {
+        protected TowerUpgradeProperties TowerUpgradeProperties { get; private set; }
+
+        public string Name { get { return TowerUpgradeProperties.name; } }
+        public int Cost { get { return TowerUpgradeProperties.UpgradeCost; } }
+        public UpgradeType UpgradeType => TowerUpgradeProperties.UpgradeType;
+        public bool IsPurchased { get; private set; }
+
+        public virtual void PurchaseUpgrade(TowerController towerController)
+        {
+            IsPurchased = true;
+            GameController.Instance.DecrementMoney(Cost);
+            towerController.IncreaseTotalTowerCost(Cost);
+        }
+
+        public static TowerUpgrade GetNewUpgrade(TowerUpgradeProperties upgradeProperties)
+        {
+            if (upgradeProperties == null) { return null; }
+            var upgradeType = upgradeProperties.UpgradeType;
+            switch (upgradeType)
+            {
+                case UpgradeType.Range:
+                    return new RangeUpgrade { TowerUpgradeProperties = upgradeProperties };
+                case UpgradeType.Cooldown:
+                    return new SpeedUpgrade() { TowerUpgradeProperties = upgradeProperties };
+                //case UpgradeType.Damage:
+                //return new DurationUpgrade() { TowerUpgradeProperties = upgradeProperties };
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class RangeUpgrade : TowerUpgrade
+    {
+        private float rangeValue => TowerUpgradeProperties._upgradeValue;
+
+        public override void PurchaseUpgrade(TowerController towerController)
+        {
+            if (towerController.EnemyDetector is IUpgradeRange rangeUpdate)
+            {
+                rangeUpdate.SetRange(rangeValue);
+                base.PurchaseUpgrade(towerController);
+            }
+
+        }
+    }
+
+    public class SpeedUpgrade : TowerUpgrade
+    {
+        private float SpeedValue => TowerUpgradeProperties._upgradeValue;
+
+        public override void PurchaseUpgrade(TowerController towerController)
+        {
+            if (towerController.ParticleControll is IUpgradeSpeed speedUpdate)
+            {
+                speedUpdate.SetFiringRate(SpeedValue);
+                base.PurchaseUpgrade(towerController);
+            }
+
+        }
+    }
+}
---
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Towers/TowerUpgrades/TowerUpgradeController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Towers.TowerUpgrades;
+using UnityEngine;
+
+namespace TowerDefense.Towers.TowerAttackControllers
+{
+    public class TowerUpgradeController : MonoBehaviour
+    {
+        private List<TowerUpgrade> _upgrades = new List<TowerUpgrade>();
+
+        public List<TowerUpgrade> Upgrades
+        {
+            get { return _upgrades; }
+            set { _upgrades = value; }
+        }
+
+        private int _upgradeIndex = -1;
+
+        public void InitializeUpgrades(TowerController controller)
+        {
+            var towerProperties = controller.Properties;
+
+            foreach (var item in towerProperties.TowerUpgradeOptions)
+            {
+                _upgrades.Add(TowerUpgrade.GetNewUpgrade(item));
+            }
+        }
+
+        public TowerUpgrade GetUpgrade()
+        {
+            _upgradeIndex++;
+            if (_upgradeIndex < _upgrades.Count)
+            {
+                return _upgrades[_upgradeIndex];
+            }
+
+            return null;
+
+
+        }
+
+    }
+}
