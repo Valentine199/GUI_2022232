8bc145a3035d2c046f22748f7d7956a6fde244d7
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-05-10 22:02:43 +0200

    PlaceTower now issues a ServerRPC

--- UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/TowerBuilderOverlay/PlaceTower.cs
+++ UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/TowerBuilderOverlay/PlaceTower.cs
@@ -3,17 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TowerDefense.Data.Towers;
 using TowerDefense.Gameplay.Core;
+using Unity.Netcode;
 //using UnityEditor.Build.Content;
 using UnityEngine;
 
-public class PlaceTower : MonoBehaviour
+public class PlaceTower : NetworkBehaviour
 {
     private void Start()
     {
         _camera = Camera.main;
     }
 
-    public void PlaceBuilding(TowerProperties towerToPlace)
+    public void HandleBuildingPlacementRequest(TowerProperties towerToPlace)
     {
         Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f,0.5f, 0));
 
@@ -21,19 +22,43 @@ public class PlaceTower : MonoBehaviour
         {
             if ((_placeableMask.value & (1 << hit.collider.gameObject.layer)) > 0)
             {
-                if (GameController.Instance.Money >= towerToPlace.TowerCost)
-                {
-                    Instantiate(towerToPlace.TowerRender, hit.point, Quaternion.identity);
-                    GameController.Instance.DecrementMoney(towerToPlace.TowerCost);
-                }
-                else
-                {
-                    Debug.Log("Not enough money.");
-                }
+                PlaceBuildingServerRPC(GetTowerListIndex(towerToPlace), hit.point);
             }  
         }        
     }
+    [ServerRpc(RequireOwnership = false)]
+    public void PlaceBuildingServerRPC(int TowerIndex, Vector3 spawnPoint)
+    {
+        TowerProperties towerToPlace = GetTowerPropFromIndex(TowerIndex);
+
+        if (GameController.Instance.Money >= towerToPlace.TowerCost)
+        {
+           
+            GameObject inst = Instantiate(towerToPlace.TowerRender, spawnPoint, Quaternion.identity);
+            NetworkObject placingTowerNetworked = inst.GetComponent<NetworkObject>();
+            placingTowerNetworked.Spawn(true);
+            
+            GameController.Instance.DecrementMoney(towerToPlace.TowerCost);
+        }
+        else
+        {
+            Debug.Log("Not enough money.");
+        }
+    }
+
+
+    //helpers
+    private int GetTowerListIndex(TowerProperties SoToFind)
+    {
+        return TowerList.TowerSOList.IndexOf(SoToFind);
+    }
+
+    private TowerProperties GetTowerPropFromIndex(int index)
+    {
+        return TowerList.TowerSOList[index];
+    }
 
     [SerializeField] private Camera _camera;
+    [SerializeField] private TowerTypeListSO TowerList;
     [SerializeField] private LayerMask _placeableMask = new LayerMask();
 }
