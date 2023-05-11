075e31b95d1e373e9c8e330df28d7958b9c52d15
Karesz <karcsibalazs@gmail.com>
2023-05-11 16:34:31 +0200

    Replace woodensigntest with TowerInteract

--- UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/InteractUI/TowerInteract.cs
+++ UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/InteractUI/TowerInteract.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class woodensigntest : MonoBehaviour,IInteractable
+public class TowerInteract : MonoBehaviour,IInteractable
 {
     public void Interact()
     {
         Debug.Log("XD");
     }
-
     // Start is called before the first frame update
     void Start()
     {
