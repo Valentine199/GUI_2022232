f237206e0966eef75b83844d877f3074e6b8421a
infopek <10saero01@gmail.com>
2023-05-13 13:23:12 +0200

    Spawn enemies on the server

--- UNITY/GUI_2022232/Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
@@ -5,6 +5,7 @@ using TowerDefense.Data.Enemies;
 using TowerDefense.Gameplay.Core;
 using TowerDefense.Gameplay.Enemies;
 using TowerDefense.Gameplay.Path;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace TowerDefense.Gameplay.Enemies
@@ -20,20 +21,9 @@ namespace TowerDefense.Gameplay.Enemies
             SpawnEnemy(enemyProperties, _pathHeadPosition, 0);
         }
 
-        public EnemyController SpawnEnemy(EnemyProperties enemyProperties, Vector3 spawnPosition, int waypointIndex)
+        public void SpawnEnemy(EnemyProperties enemyProperties, Vector3 spawnPosition, int waypointIndex)
         {
-            var newEnemy = Instantiate(_enemyPrefabs[(int)enemyProperties.EnemyType], spawnPosition, Quaternion.identity);
-            var newEnemyController = newEnemy.GetComponent<EnemyController>();
-
-            newEnemyController.PathController = _pathController;
-            newEnemyController.InitEnemy(enemyProperties, waypointIndex);
-            OnEnemySpawned?.Invoke(newEnemyController);
-
-            if (_spawnedEnemiesGO == null)
-                _spawnedEnemiesGO = new GameObject("Enemies");
-            newEnemy.transform.parent = _spawnedEnemiesGO.transform;
-
-            return newEnemyController;
+            EnemySpawnerMultiplayer.Instance.SpawnEnemy(enemyProperties, spawnPosition, waypointIndex);
         }
 
         public static EnemySpawner Instance
@@ -49,8 +39,6 @@ namespace TowerDefense.Gameplay.Enemies
                 _instance = value;
             }
         }
-
-        public event Action<EnemyController> OnEnemySpawned;
 
         private void Start()
         {
