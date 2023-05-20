e48db86440dbbb7525d77c3893eaa45ba61c8015
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-05-20 16:35:12 +0200

    TowerInteract handles button pushes, UI

---
+++ UNITY/GUI_2022232/Assets/TowerInteractUI.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Towers;
+using TowerDefense.Towers.TowerAttackControllers;
+using TowerDefense.Towers.TowerUpgrades;
+using UnityEngine;
+using TMPro;
+using Unity.Netcode;
+using TowerDefense.Towers.TowerEnums;
+
+public class TowerInteractUI : MonoBehaviour
+{
+    private IInteractable _towerManager;
+    public IInteractable TowerManager
+    {
+        get
+        {
+            return _towerManager;
+        }
+        set
+        {
+            _towerManager = value;
+
+        }
+    }
+
+    [SerializeField] private TMP_Text _upgradeText;
+    [SerializeField] private TMP_Text _upgradeCost;
+
+    [SerializeField] private TMP_Text _SellPrice;
+
+    [SerializeField] private TMP_Text _prevTargeting;
+    [SerializeField] private TMP_Text _currentTargeting;
+    [SerializeField] private TMP_Text _nextTargeting;
+
+
+    public void InitSelf(IInteractable interactable)
+    {
+        _towerManager = interactable;
+        ShowTowerInfo(_towerManager.GetUpgradeInfo());
+        _towerManager.OnNewUpgrade += ShowTowerInfo;
+        _towerManager.OnTargetingStyleChange += ChangeTargetingText;
+        ChangeTargetingText();
+    }
+
+    private void ShowTowerInfo(TowerUpgrade obj)
+    {
+        if(obj != null)
+        {
+            _upgradeText.text = obj.UpgradeName;
+            _upgradeCost.text = obj.Cost.ToString() + "$";
+        }
+        else
+        {
+            _upgradeText.text = "Fully upgraded";
+            _upgradeCost.text = string.Empty;
+        }
+
+        _SellPrice.text = _towerManager.GetSellPrice().ToString() +"$";
+    }
+
+    private void ChangeTargetingText()
+    {
+        _currentTargeting.text = _towerManager.GetTargetingInfo().ToString();
+
+
+        int val = (int)_towerManager.GetTargetingInfo();
+        int prevVal = val;
+        val++;
+        if (val >= Enum.GetNames(typeof(TargetingStyle)).Length)
+        {
+            val = 0;
+        }
+
+        _nextTargeting.text = ((TargetingStyle)val).ToString();
+
+        val = prevVal;
+        val--;
+        if (val < 0)
+        {
+            val = Enum.GetNames(typeof(TargetingStyle)).Length - 1;
+        }
+        _prevTargeting.text = ((TargetingStyle)val).ToString();
+        
+
+    }
+
+    public void NextTargeting()
+    {
+        _towerManager.CycleTargetingStyleForward();
+
+        //int val = (int)_towerManager.GetTargetingInfo();
+        //val++;
+        //if (val >= Enum.GetNames(typeof(TargetingStyle)).Length)
+        //{
+        //    val = 0;
+        //}
+        
+        //_currentTargeting.text = ((TargetingStyle)val).ToString();
+        
+    }
+
+    public void PrevTargeting()
+    {
+        _towerManager.CycleTargetingStyleBackwards();
+
+        //int val = (int)_towerManager.GetTargetingInfo();
+        //val--;
+        //if (val < 0)
+        //{
+        //    val = Enum.GetNames(typeof(TargetingStyle)).Length - 1;
+        //}
+        //_currentTargeting.text = ((TargetingStyle)val).ToString();
+        //_currentTargeting.text = _towerManager.GetTargetingInfo().ToString();
+    }
+
+    public void BuyUpgrade()
+    {
+        _towerManager.InteractUpgradeServerRpc();
+    }
+
+    public void Sell()
+    {
+        _towerManager.SellTower();
+    }
+}
