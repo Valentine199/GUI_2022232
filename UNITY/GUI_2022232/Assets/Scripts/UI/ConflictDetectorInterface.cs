5e199a9d37faa601048ef20db46ec95a52c5df89
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-05-24 12:51:10 +0200

    only one UI element can be active at a time

---
+++ UNITY/GUI_2022232/Assets/Scripts/UI/ConflictDetectorInterface.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public interface ConflictDetectorInterface
+{
+    public bool OtherIsOpen();
+}
