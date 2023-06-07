dd928cb920db8802274a6f758156f0fc6ecbae4d
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-06-07 15:59:00 +0200

    enemy can be poisoned

--- UNITY/GUI_2022232/Assets/Scripts/Gameplay/Towers/TowerAttackControllers/PoisonDamage.cs
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Towers/TowerAttackControllers/PoisonDamage.cs
@@ -1,11 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 using TowerDefense.Data.Towers;
 using TowerDefense.Gameplay.Enemies;
 using TowerDefense.Towers.TowerAttackControllers;
 using Unity.Netcode;
 using UnityEngine;
 
-public class FireDamage : TowerDamage
+public class PoisonDamage : TowerDamage
 {
     public TowerEffectProperties EffectProperties { get; private set; }
 
@@ -18,19 +19,19 @@ public class FireDamage : TowerDamage
     IEnumerator ApplyEffect(EnemyController enemy)
     {
         WaitForSeconds waitTime = new WaitForSeconds(EffectProperties.TickSpeed);
-
         GameObject ps = Instantiate(EffectProperties.ParticleEffect, enemy.transform);
         NetworkObject psNetworkObject = ps.GetComponent<NetworkObject>();
         psNetworkObject.Spawn();
 
         psNetworkObject.transform.parent = enemy.transform;
         enemy.AddActiveEffect(ps);
-        enemy.SetOnFire();
+        enemy.IsPoisoned = true;
         //SpawnFireEffectServerRpc(enemy.GetEnemyNetworkObject());
 
         float startTime = Time.time;
         float currentTime = Time.time;
 
+        enemy.CurrentMoveSpeed -= EffectProperties.SpeedReduction;
 
         while ((currentTime - startTime) < EffectProperties.Duration && enemy.HealthRemaining > 0)
         {
@@ -42,7 +43,8 @@ public class FireDamage : TowerDamage
         if (enemy.HealthRemaining > 0)
         {
             enemy.RemoveActiveEffect(ps);
-            enemy.RemoveFire();
+            enemy.CurrentMoveSpeed += EffectProperties.SpeedReduction;
+            enemy.IsPoisoned = false;
             NetworkObject psNetwork = ps.GetComponent<NetworkObject>();
             psNetwork.Despawn();
             Destroy(ps);
@@ -59,11 +61,11 @@ public class FireDamage : TowerDamage
     {
         base.DoDamage(enemy);
 
-        if (!enemy.IsOnFire)
+        if (!enemy.IsPoisoned)
         {
             //Debug.Log("FireDamage");
             StartCoroutine(ApplyEffect(enemy));
-           
+
         }
     }
 }
