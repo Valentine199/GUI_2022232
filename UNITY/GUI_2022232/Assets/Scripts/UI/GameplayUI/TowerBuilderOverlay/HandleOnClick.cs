038fb81cadcac590eb341f65376a69db73668bdf
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-06-08 12:24:42 +0200

    shooting sound selecting building to be placed

--- UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/TowerBuilderOverlay/HandleOnClick.cs
+++ UNITY/GUI_2022232/Assets/Scripts/UI/GameplayUI/TowerBuilderOverlay/HandleOnClick.cs
@@ -3,14 +3,20 @@ using UnityEngine.EventSystems;
 using System;
 using TowerDefense.Data.Towers;
 
-public class HandleOnClick : MonoBehaviour, IPointerDownHandler
+public class HandleOnClick : MonoBehaviour//, IPointerDownHandler
 {
     public event Action OnClickSetCanvas;
     public event Action<TowerProperties> OnClickPlaceBuilding;
     
     public TowerProperties MyTower;
 
-    public void OnPointerDown(PointerEventData eventData)
+    ////public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    OnClickSetCanvas?.Invoke();
+    //    OnClickPlaceBuilding?.Invoke(MyTower);
+    //}
+
+    public void ElementClicked()
     {
         OnClickSetCanvas?.Invoke();
         OnClickPlaceBuilding?.Invoke(MyTower);
