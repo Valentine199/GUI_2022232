62f5d83ceddade7f5467e2ab208e28a83c89463f
Karesz <karcsibalazs@gmail.com>
2023-06-08 18:21:06 +0200

    Add Recoil script

--- UNITY/GUI_2022232/Assets/Scripts/Gameplay/Core/PlayerController.cs
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Core/PlayerController.cs
@@ -356,5 +356,27 @@ namespace TowerDefense.Gameplay.Core
         {
             AudioListener.volume = PlayerPrefs.GetFloat("volume");
         }
+
+        //private Vector3 currentRotation;
+        //private Vector3 targetRotation;
+
+        //[SerializeField] private float recoilX;
+        //[SerializeField] private float recoilY;
+        //[SerializeField] private float recoilZ;
+
+        //[SerializeField] private float snappiness;
+        //[SerializeField] private float returnSpeed;
+
+        //void Update()
+        //{
+        //    targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, returnSpeed * Time.deltaTime);
+        //    currentRotation = Vector3.Slerp(currentRotation, targetRotation, snappiness * Time.fixedDeltaTime);
+        //    Camera.localRotation = Quaternion.Euler(currentRotation);
+        //}
+
+        //public void RecoilFire()
+        //{
+        //    targetRotation += new Vector3(recoilX, UnityEngine.Random.Range(-recoilY, recoilY), UnityEngine.Random.Range(-recoilZ, recoilZ));
+        //}
     }
 }
---
+++ UNITY/GUI_2022232/Assets/Scripts/Gameplay/Guns/Recoil.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+
+    [SerializeField] private float recoilX;
+    [SerializeField] private float recoilY;
+    [SerializeField] private float recoilZ;
+    
+    [SerializeField] private float snappiness;
+    [SerializeField] private float returnSpeed;
+   
+    void Update()
+    {
+        targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, returnSpeed * Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation, targetRotation, snappiness * Time.fixedDeltaTime);
+        transform.localRotation = Quaternion.Euler(currentRotation);          
+    }
+
+    public void RecoilFire()
+    {
+        targetRotation += new Vector3(recoilX, Random.Range(-recoilY, recoilY), Random.Range(-recoilZ, recoilZ));
+    }
+}
